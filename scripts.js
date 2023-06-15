2f8f69b3a41d1d221cda53aec49105513eca916c
jlbarn11 <24995533+jlbarn11@users.noreply.github.com>
2023-06-14 20:22:50 -0700

    Completed layout styles/JS

--- scripts.js
+++ scripts.js
@@ -1,11 +1,22 @@
 // To attach event listeners to the cards
-let cards = document.getElementsByTagName("li");
+function addClickListener(card){
+    
+    let link = document.querySelector(`${card} > a`);
+    if (link !== e.target) {
+        link.click()
+    }
+}
 
-for(let card of cards){
-    card.addEventListener(e => {  
-        let link = document.querySelector(`${card} > a`);
-        if (link !== e.target) {
-            link.click()
+// get all the cards on the page
+let cards = document.querySelectorAll("main li");
+// get all the links in the cards
+let links = document.querySelectorAll("li a");
+
+// attach event listener
+for(let i = 0; i < cards.length; i++){
+    cards[i].addEventListener("click", function(e){
+        if(links[i] !== e.target){
+            links[i].click();
         }
     });
 }
